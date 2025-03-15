6a7446cfac1cbfd8c967b659381b2a08521b0b08
Gustavo Gil <gustavo.gil@sptech.school>
2025-03-15 09:30:15 -0300

    resolvi, perdão JP ;-;

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div>
-      <h1>Welcome to the Home page!</h1>
+      <h1 className="text-blue-500">Welcome to the Home page!</h1>
     </div>
   );
 
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -10,7 +10,7 @@ function Register() {
   }, []);
 
   return (
-    <main className="flex items-center justify-center min-h-screen w-full bg-gradient-to-b from-[#060607] to-[#11113e] text-[white] font-sans">
+    <main className="flex items-center justify-center min-h-screen w-full bg-gradient-to-b from-[#060607] to-[#11113e] text-white font-sans">
       <section className="flex flex-col items-center justify-start gap-6 w-full px-4 py-8">
         <img src={logo} alt="logo-studio-zero-header" className="h-[250px]" />
         <h1 className="font-medium text-4xl tracking-widest text-[#056f5b] text-center">
@@ -20,48 +20,48 @@ function Register() {
       <form className="flex flex-col items-center gap-10 w-full px-4 py-8">
         <section className="flex flex-wrap items-center justify-between w-full gap-4">
           <div className="flex flex-col justify-between w-full sm:w-[48%] md:w-[24%]">
-            <label className="text-sm text-[white]">Nome</label>
+            <label className="text-sm text-white">Nome</label>
             <input
               id="input_nome"
               type="text"
               autoComplete="off"
-              className="h-8 bg-transparent border-b border-[#5f6176] text-[white] text-lg focus:outline-none transition-all duration-150 ease-in-out"
+              className="h-8 bg-transparent border-b border-[#5f6176] text-white text-lg focus:outline-none transition-all duration-150 ease-in-out"
             />
           </div>
           <div className="flex flex-col justify-between w-full sm:w-[48%] md:w-[24%]">
-            <label className="text-sm text-[white]">CPF</label>
+            <label className="text-sm text-white">CPF</label>
             <input
               id="input_cpf"
               type="text"
               maxLength="14"
               autoComplete="off"
-              className="h-8 bg-transparent border-b border-[#5f6176] text-[white] text-lg focus:outline-none transition-all duration-150 ease-in-out"
+              className="h-8 bg-transparent border-b border-[#5f6176] text-white text-lg focus:outline-none transition-all duration-150 ease-in-out"
             />
           </div>
           <div className="flex flex-col justify-between w-full sm:w-[48%] md:w-[24%]">
-            <label className="text-sm text-[white]">Email</label>
+            <label className="text-sm text-white">Email</label>
             <input
               id="input_email"
               type="email"
               autoComplete="off"
-              className="h-8 bg-transparent border-b border-[#5f6176] text-[white] text-lg focus:outline-none transition-all duration-150 ease-in-out"
+              className="h-8 bg-transparent border-b border-[#5f6176] text-white text-lg focus:outline-none transition-all duration-150 ease-in-out"
             />
           </div>
           <div className="flex flex-col justify-between w-full sm:w-[48%] md:w-[24%]">
-            <label className="text-sm text-[white]">Telefone</label>
+            <label className="text-sm text-white">Telefone</label>
             <input
               id="input_telefone"
               type="text"
               maxLength="15"
               autoComplete="off"
-              className="h-8 bg-transparent border-b border-[#5f6176] text-[white] text-lg focus:outline-none transition-all duration-150 ease-in-out"
+              className="h-8 bg-transparent border-b border-[#5f6176] text-white text-lg focus:outline-none transition-all duration-150 ease-in-out"
             />
           </div>
         </section>
         <button
           id="btn_confirm"
           disabled
-          className="h-[55px] w-full max-w-[550px] border-2 border-[#33343f] rounded-full cursor-not-allowed text-xl tracking-widest bg-[#530d3e] text-[white] transition-all duration-300 ease-in-out disabled:bg-[#33343f] disabled:text-[#5f6176]"
+          className="h-[55px] w-full max-w-[550px] border-2 border-[#33343f] rounded-full cursor-not-allowed text-xl tracking-widest bg-[#530d3e] text-white transition-all duration-300 ease-in-out disabled:bg-[#33343f] disabled:text-[#5f6176]"
         >
           Confirmar
         </button>
