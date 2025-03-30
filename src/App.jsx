86e5298bbc9d2289c42dc8680b0d901cb34b4a7b
Gustavo Gil <gustavo.gil@sptech.school>
2025-03-30 16:48:33 -0300

    alteração background

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import AppRoutes from "./routes";
 import Navbar from "./components/Navbar/Navbar";
-import Background from "./assets/background.png";
+import Background from "./assets/background_backoffice_studiozero.mp4";
 import { Toaster } from "react-hot-toast"; // <-- lib de notificação bunitinha :3
 import GlobalProvider from "./context/GlobalProvider"; // <-- importa o provider
 
@@ -8,17 +8,21 @@ function App() {
   return (
     //UserProvider tbm vem do contexto, precisamos envolver ele na aplicação para o contexto funcionar em toda a aplicação
     <GlobalProvider>
-      <div
-        className="min-h-screen min-w-full bg-black"
-        style={{
-          background: `url(${Background}) no-repeat center center/cover`,
-        }}
-      >
-        <Navbar />
-        <AppRoutes />
-        {/* //Toaster é só pra n usar o alert, ai fica mais bonitinho e n perde
-        tempo estilizando o alert */}
-        <Toaster position="top-center" reverseOrder={false} />
+      <div className="min-h-screen min-w-full bg-black relative">
+        <video
+          className="absolute top-0 left-0 w-full h-full object-cover"
+          src={Background}
+          autoPlay
+          loop
+          muted
+        />
+        <div className="relative z-10">
+          <Navbar />
+          <AppRoutes />
+          {/* //Toaster é só pra n usar o alert, ai fica mais bonitinho e n perde
+          tempo estilizando o alert */}
+          <Toaster position="top-center" reverseOrder={false} />
+        </div>
       </div>
     </GlobalProvider>
   );
