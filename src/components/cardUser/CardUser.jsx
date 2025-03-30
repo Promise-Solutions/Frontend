71f9f167f86cc5a7ddd48094925e40bbb3ff9a1f
Gustavo Gil <gustavo.gil@sptech.school>
2025-03-30 15:20:34 -0300

    hover no card User

--- src/components/cardUser/CardUser.jsx
+++ src/components/cardUser/CardUser.jsx
@@ -10,7 +10,7 @@ const CardUser = ({
   return (
     <div
       id={`user_${id}`}
-      className={`card_user border-1 border-pink-zero text-[#d9d9d9] w-[17rem] h-auto rounded-[5px] bg-[#1E1E1E90] cursor-pointer hover:border-[cyan-zero] transition duration-100 ease-in-out `}
+      className={`card_user border-1 border-pink-zero text-[#d9d9d9] w-[17rem] h-auto rounded-[5px] bg-[#1E1E1E90] cursor-pointer hover:border-cyan-zero transition duration-100 ease-in-out `}
       onClick={onClick} // Trigger the onClick callback
     >
       <div className="px-4 py-6 text-2xl font-bold">
@@ -18,7 +18,7 @@ const CardUser = ({
       </div>
       <div>
         {/* Divisão */}
-        <div className={`border-1 border-pink-zero transition duration-100`}></div>
+        <div className={`border-1 border-pink-zero hover:border-cyan-zero transition duration-100`}></div>
         <ul
           className={`px-8 py-6 text-[16px] list-disc marker:text-cyan-zero ease-in-out`}
         >
