e7f6abd08086fc7013b0ea132687a4f27d52fae6
Gustavo Gil <gustavo.gil@sptech.school>
2025-04-23 19:05:00 -0300

    retirando alert da commanda

--- src/pages/Command/Command.jsx
+++ src/pages/Command/Command.jsx
@@ -2,15 +2,6 @@ import { useEffect } from "react";
 import { RenderCommandDetails } from "./Command.script.jsx";
 
 const Command = () => {
-  useEffect(() => {
-    console.log("Command component mounted");
-    alert("Command page loaded");
-
-    return () => {
-      console.log("Command component unmounted");
-    };
-  }, []); // Ensure this runs only once when the component is mounted
-
   return (
     <div className="text-white flex mx-16 flex-col">
       <RenderCommandDetails />
