8e6eda387b489ad1ed35b8648ce03c9acf5ca74d
Gustavo Gil <gustavo.gil@sptech.school>
2025-05-22 20:16:17 -0300

    alerações filtros

--- src/pages/Reports/Reports.jsx
+++ src/pages/Reports/Reports.jsx
@@ -74,85 +74,102 @@ const Reports = () => {
   };
 
   return (
-    <div className="mx-16 my-6 text-white">
-      <div className="flex justify-end">
-        <PrimaryButton
-          id="generate_report_button_id"
-          text="Gerar Relatório"
-          onClick={() => {}}
-        />
-      </div>
-      {/* Título e instruções */}
-      <div className="text-center space-y-2 mb-10">
-        <div className="flex justify-center">
-          <div className="bg-white/10 p-4 rounded-full">
-            <CiFileOn size={"70px"} />
+    <>
+      {/* CSS para deixar o ícone do calendário branco */}
+      <style>
+        {`
+          input[type="date"]::-webkit-calendar-picker-indicator {
+            filter: invert(1);
+          }
+          input[type="date"]::-ms-input-placeholder { color: #fff; }
+          input[type="date"]::-webkit-input-placeholder { color: #fff; }
+          input[type="date"]::-moz-placeholder { color: #fff; }
+          input[type="date"]:-ms-input-placeholder { color: #fff; }
+          input[type="date"]::placeholder { color: #fff; }
+        `}
+      </style>
+      <div className="mx-16 my-6 text-white">
+        <div className="flex justify-between mb-4">
+          {/* Título e instruções */}
+          <div className="text-center pl-50 w-[85%] space-y-2 mb-4">
+            <div className="flex justify-center">
+              <div className="bg-white/10 p-4 rounded-full">
+                <CiFileOn size={"70px"} />
+              </div>
+            </div>
+            <h1 className="text-3xl font-bold">Gerar Relatório</h1>
+            <p className="text-gray-400 text-sm">
+              Você pode gerar um relatório agora ou visualizar os relatórios já
+              existentes.
+            </p>
+          </div>
+          <div className="flex">
+            <PrimaryButton
+              id="generate_report_button_id"
+              text="Gerar Relatório"
+              onClick={() => {}}
+            />
           </div>
         </div>
-        <h1 className="text-3xl font-bold">Gerar Relatório</h1>
-        <p className="text-gray-400 text-sm">
-          Você pode gerar um relatório agora ou visualizar os relatórios já
-          existentes.
-        </p>
-      </div>
-      <div className="flex flex-wrap justify-start items-end gap-4 mb-10">
-        <PrimaryButton
-          id="filter_report_button_id"
-          text="Filtrar"
-          onClick={handleFilter}
-        />
-        <label className="flex flex-col text-gray-400">
-          <span>Período de: 00/00/0000</span>
-          <input
-            type="date"
-            className="bg-black border border-pink-zero text-white px-4 py-2 w-[250px] focus:outline-none"
-            value={dateFrom}
-            onChange={(e) => setDateFrom(e.target.value)}
+        <div className="flex flex-wrap justify-start items-end gap-4 mb-10">
+          <PrimaryButton
+            id="filter_report_button_id"
+            text="Filtrar"
+            onClick={handleFilter}
           />
-        </label>
-        <label className="flex flex-col text-gray-400">
-          <span>Período até: 00/00/0000</span>
-          <input
-            type="date"
-            className="bg-black border border-pink-zero text-white px-4 py-2 w-[250px] focus:outline-none"
-            value={dateTo}
-            onChange={(e) => setDateTo(e.target.value)}
-          />
-        </label>
-        <button
-          id="clear_filter_report_button_id"
-          className="flex items-center justify-center gap-2 cursor-pointer text-pink-zero hover:text-cyan-zero transition text-3xl p-2"
-          onClick={handleClearFilters}
-          title="Remover Filtros"
-          style={{ lineHeight: 0 }}
-        >
-          <CiEraser size={"40px"} />
-        </button>
-      </div>
-      <div className="max-h-[450px] overflow-y-auto pr-2">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-          {/* Overlay para fechar o menu ao clicar fora */}
-          {openMenuIndex !== null && (
-            <div
-              className="fixed inset-0 z-0"
-              onClick={() => setOpenMenuIndex(null)}
-              style={{ cursor: "default" }}
+          <label className="flex flex-col text-gray-400">
+            <span>Período de: 00/00/0000</span>
+            <input
+              type="date"
+              className="bg-black border border-pink-zero text-white px-4 py-2 w-[250px] focus:outline-none"
+              value={dateFrom}
+              onChange={(e) => setDateFrom(e.target.value)}
             />
-          )}
-          {reports.map((item, index) => (
-            <CardReport
-              key={index}
-              id={index}
-              item={item}
-              isMenuOpen={openMenuIndex === index}
-              onMenuOpen={() => handleMenuOpen(index)}
-              onDownload={() => handleDownload(index)}
-              onDelete={() => handleDelete(index)}
+          </label>
+          <label className="flex flex-col text-gray-400">
+            <span>Período até: 00/00/0000</span>
+            <input
+              type="date"
+              className="bg-black border border-pink-zero text-white px-4 py-2 w-[250px] focus:outline-none"
+              value={dateTo}
+              onChange={(e) => setDateTo(e.target.value)}
             />
-          ))}
+          </label>
+          <button
+            id="clear_filter_report_button_id"
+            className="flex items-center justify-center cursor-pointer text-pink-zero hover:text-cyan-zero transition text-3xl"
+            onClick={handleClearFilters}
+            title="Remover Filtros"
+            style={{ lineHeight: 0 }}
+          >
+            <CiEraser size={"40px"} />
+          </button>
+        </div>
+        <div className="max-h-[450px] overflow-y-auto pr-2">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            {/* Overlay para fechar o menu ao clicar fora */}
+            {openMenuIndex !== null && (
+              <div
+                className="fixed inset-0 z-0"
+                onClick={() => setOpenMenuIndex(null)}
+                style={{ cursor: "default" }}
+              />
+            )}
+            {reports.map((item, index) => (
+              <CardReport
+                key={index}
+                id={index}
+                item={item}
+                isMenuOpen={openMenuIndex === index}
+                onMenuOpen={() => handleMenuOpen(index)}
+                onDownload={() => handleDownload(index)}
+                onDelete={() => handleDelete(index)}
+              />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
