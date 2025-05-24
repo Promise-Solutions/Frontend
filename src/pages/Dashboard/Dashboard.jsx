f512b82a3dedb3d3ba8ab09153aaae9edcb9836d
Gustavo Gil <gustavo.gil@sptech.school>
2025-05-24 19:33:10 -0300

    atualizando dados na dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -8,23 +8,60 @@ import { useEffect, useState } from "react";
 import { axiosProvider } from "../../provider/apiProvider";
 import { SyncLoader } from "react-spinners";
 import { formatDateWithoutTime } from "../../hooks/formatUtils";
+import { IoMdRefresh } from "react-icons/io";
+import { showToast } from "../../components/toastStyle/ToastStyle";
 
 const Dashboard = () => {
   const [lastDateUpdate, setLastDateUpdate] = useState();
   const [isLoading, setIsLoading] = useState(true);
+  const [renderKey, setRenderKey] = useState(Date.now());
 
-  useEffect(() => {
+  function handleRefresh() {
+    setIsLoading(true);
     axiosProvider.get("/dashboard/recent-time").then((response) => {
       const lastUpdate = response.data;
       setLastDateUpdate(formatDateWithoutTime(lastUpdate));
     });
-
     setIsLoading(false);
+  }
+
+  function handleRefreshAndCleanTrancing() {
+    setIsLoading(true);
+    axiosProvider.get("/dashboard/recent-time").then((response) => {
+      const lastUpdate = response.data;
+      setLastDateUpdate(formatDateWithoutTime(lastUpdate));
+    });
+    axiosProvider.delete("/tracing");
+    setIsLoading(false);
+    setRenderKey(Date.now());
+    showToast.success("Dados atualizados com sucesso!");
+  }
+
+  useEffect(() => {
+    handleRefresh();
   }, []);
+
 
   return (
     <div className="slide-in-ltr flex flex-col min-h-screen py-2 md:pt-2 px-6 bg-gradient-to-br from-zinc-950 to-zinc-900 text-white">
       <h1 className="text-2xl font-thin">Visualize as análises</h1>
+      <div className="flex justify-between font-bold mb-4 text-gray-400">
+        <p className="flex font-bold text-yellow-zero">
+          Os dados existentes abaixo, são referentes aos dados atuais do
+          sistema.
+        </p>
+        <span className="flex text-gray-400 text-sm">
+          <span
+            onClick={() => handleRefreshAndCleanTrancing()}
+            title="Atualizar"
+            className="text-pink-zero mr-1 cursor-pointer hover:text-cyan-zero transition-all duration-300 hover:scale-110 hover:rotate-360">
+            <IoMdRefresh size={"24px"}/>
+          </span>
+          {`Última atualização: ${
+            lastDateUpdate ? lastDateUpdate : "Carregando..."
+          }`}
+        </span>
+      </div>
       {isLoading ? (
         <div className="w-full h-full flex items-center justify-center ">
           <SyncLoader
@@ -35,16 +72,7 @@ const Dashboard = () => {
           />
         </div>
       ) : (
-        <>
-          <div className="flex justify-between font-bold mb-4 text-gray-400">
-            <p className="flex font-bold text-yellow-zero">
-              Os dados existentes abaixo, são referentes aos dados atuais do
-              sistema.
-            </p>
-            {`Última atualização: ${
-              lastDateUpdate ? lastDateUpdate : "Carregando..."
-            }`}
-          </div>
+        <div key={renderKey}>
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
             <FrequencyGraphic title="Quantidade de Atendimentos Totais" />
             <ActiveUsersGraphic title="Clientes Ativos Totais" />
@@ -53,7 +81,7 @@ const Dashboard = () => {
             <BarGraphic title="Finanças Totais do Bar" />
             <ProfitGraphic title="Finanças Gerais Totais" />
           </div>
-        </>
+        </div>
       )}
     </div>
   );
